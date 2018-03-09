7de37333465dc4f7797d7e60186c3eea5a999885
Zach Gulde <zachgulde@gmail.com>
2018-03-09 11:11:03 -0600

    Suppress Warnings

--- src/Author.java
+++ src/Author.java
@@ -16,14 +16,17 @@ public class Author extends Person {
      * @deprecated Use publishedBooks instead
      */
     @Deprecated
+    @SuppressWarnings("unchecked")
     public List<String> getBooks() {
         return books;
     }
 
+    @SuppressWarnings("unchecked")
     public List<String> publishedBooks() {
         return books;
     }
 
+    @SuppressWarnings("unchecked")
     public void addBook(String book) {
         books.add(book);
     }
